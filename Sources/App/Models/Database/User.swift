5263dfbf0654efcfb5844a85e2fc062a72bcc112
Alexey Plekhanov <alexey.plekhanov@gmail.com>
2020-07-07 03:24:39 -0700

    Add User database model.

---
+++ Sources/App/Models/Database/User.swift
@@ -0,0 +1,35 @@
+import Vapor
+import Fluent
+
+final class User: Model {
+
+    static let schema = "users"
+
+    init() {}
+
+    init(_ email: String, _ firstName: String? = nil, _ lastName: String? = nil, _ password: String) throws {
+        self.email = email
+        self.firstname = firstName
+        self.lastname = lastName
+        self.password = try BCryptDigest().hash(password)
+    }
+
+    @ID(key: "id")
+    var id: UUID?
+
+    @Field(key: "firstname")
+    var firstname: String?
+    @Field(key: "lastname")
+    var lastname: String?
+    @Field(key: "email")
+    var email: String
+    @Field(key: "password")
+    var password: String
+
+    @Timestamp(key: "createdAt", on: .create)
+    var createdAt: Date?
+    @Timestamp(key: "updatedAt", on: .update)
+    var updatedAt: Date?
+    @Timestamp(key: "deletedAt", on: .delete)
+    var deletedAt: Date?
+}
