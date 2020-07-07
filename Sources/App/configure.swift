d32c4161c660f8860a8e126a66a29670e3db67d4
Alexey Plekhanov <alexey.plekhanov@gmail.com>
2020-07-07 03:31:08 -0700

    Add User migration and configure it.

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,17 +1,29 @@
 import Vapor
 import SimpleJWTMiddleware
+import Fluent
+import FluentSQLiteDriver
 
 // configures your application
 public func configure(_ app: Application) throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    
+    // Middlewares
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     app.middleware.use(CORSMiddleware())
     
+    
+    // Database and migrations
+    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+    
+    app.migrations.add(CreateUser())
+    
+    
+    // Authorization
     guard let jwksString = Environment.process.JWKS else {
         fatalError("No value was found at the given public key environment 'JWKS'")
     }
     
     try app.jwt.signers.use(jwksJSON: jwksString)
+    
     
     // register routes
     try routes(app)
