291c9cb6827d548a17b25e1a05dd806307ed6e96
Alexey Plekhanov <alexey.plekhanov@gmail.com>
2020-07-10 05:24:21 -0700

    Init service tests.

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -2,14 +2,17 @@
 import XCTVapor
 
 final class AppTests: XCTestCase {
-    func testHelloWorld() throws {
+    
+    let rootPath = "/*/users/"
+    
+    func testHealth() throws {
         let app = Application(.testing)
         defer { app.shutdown() }
         try configure(app)
-
-        try app.test(.GET, "hello") { res in
+        try app.test(.GET, rootPath.appending("health")) { res in
             XCTAssertEqual(res.status, .ok)
-            XCTAssertEqual(res.body.string, "Hello, world!")
+            XCTAssertEqual(res.body.string, "OK")
         }
     }
+    
 }
